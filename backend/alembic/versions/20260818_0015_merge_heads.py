773ad2d7429b2f9225e1afaa5ac6ad787453e483
Rahul Mahato <rahulmahato10101@gmail.com>
2026-08-18 00:06:45 +0530

    alembic: merge heads 20260804_0014 and 79324be179d9

---
+++ backend/alembic/versions/20260818_0015_merge_heads.py
@@ -0,0 +1,17 @@
+"""merge heads"""
+
+revision = "20260818_0015"
+down_revision = ("20260804_0014", "79324be179d9")
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
